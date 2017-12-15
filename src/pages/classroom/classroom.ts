7ece29818c0fa1e8e1be8a3e8343f4fa84ee653c
杨月 <83718701@qq.com>
2017-12-15 15:15:26 +0800

    添加头脑风暴、贴吧等页的链接

--- src/pages/classroom/classroom.ts
+++ src/pages/classroom/classroom.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
 import {GroupingPage} from "../grouping/grouping";
 import {DecisionPage} from "../decision/decision";
-
+import {TounaofbPage} from "../tounaofb/tounaofb";
+import {DanmuPage} from "../danmu/danmu";
+import {BaidutbPage} from "../baidutb/baidutb";
+import {WeiBoPage} from "../weibo/weibo";
+import {QQPage} from "../qq/qq";
 
 @IonicPage()
 @Component({
@@ -10,7 +14,17 @@ import {DecisionPage} from "../decision/decision";
   templateUrl: 'classroom.html',
 })
 export class ClassroomPage {
-  items = [{name:'事件起因',type: '0'},{name:'第一次决策',type:'1'},{name:'第一次场景推演',type:'2'},{name:'第一次头脑风暴',type:'3'},{name:'第二次决策',type:'1'}]
+  items = [
+    {name:'事件起因',type: '0'},
+    {name:'第一次决策',type:'1'},
+    {name:'第一次场景推演',type:'2'},
+    {name:'第一次头脑风暴',type:'3'},
+    {name:'百度贴吧',type:'4'},
+    {name:'弹幕',type:'5'},
+    {name:'新浪微博',type:'6'},
+    {name:'QQ',type:'7'},
+    {name:'第二次决策',type:'1'}
+    ]
   constructor(public navCtrl: NavController, public navParams: NavParams,public toastCtrl:ToastController) {
   }
   showToast(position: string, text: string) {
@@ -33,6 +47,21 @@ export class ClassroomPage {
     console.log(type)
     if(type == '1'){
       this.navCtrl.push(DecisionPage)
+    }
+    if(type == '3'){
+      this.navCtrl.push(TounaofbPage)
+    }
+    if(type == '4'){
+      this.navCtrl.push(BaidutbPage)
+    }
+    if(type == '5'){
+      this.navCtrl.push(DanmuPage)
+    }
+    if(type == '6'){
+      this.navCtrl.push(WeiBoPage)
+    }
+    if(type == '7'){
+      this.navCtrl.push(QQPage)
     }
   }
 }
