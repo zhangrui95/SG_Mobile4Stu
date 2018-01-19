bb966b44ebba5acf2f11332896c45f23b9f3f766
Administrator <112>
2018-01-19 14:03:33 +0800

    fixed issue reconnect

--- src/pages/danmu/danmu.ts
+++ src/pages/danmu/danmu.ts
@@ -127,12 +127,27 @@ export class DanmuPage {
   ionViewDidLoad() {
     // JSON.parse()
 
-    this.result=JSON.parse(this.s_data[0].s_data)
-    this.common=this.result['componentList'][0].data.fillData;
+    // this.result=JSON.parse(this.s_data[0].s_data)
+    // this.common=this.result['componentList'][0].data.fillData;
+    this.result = JSON.parse(this.s_data[0].s_data)
+    this.common = this.result['componentList'];
+    for (let com of  this.common ) {
+      let name = com.name;
+
+      if (name == 'video') {
+        this.name=name;
+        this.src=com.data.src;
+
+      }
+      else {
+
+      }
+
+    }
 
     this.title=this.common.title;
     this.content=this.common.content;
-
+    // this.src=com.data.src;
     // this.title='范德萨的发生非法违法文文';
     // this.content='范德萨的发生非法违法文文范德萨的发生非法违法文文范德萨的发生非法违法文文范德萨的发生非法违法文文范德萨的发生非法违法文文范德萨的发生非法违法文文范德萨的发生非法违法文文范德萨的发生非法违法文文范德萨的发生非法违法文文范德萨的发生非法违法文文';
 
